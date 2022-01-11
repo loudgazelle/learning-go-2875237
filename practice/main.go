d3dac6ecebad38d377b571fb35f4035c82eb46a7
loudgazelle <loudgazelle@users.noreply.github.com>
2022-01-11 17:39:16 -0600

    practice 02_08

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
 
+	fmt.Println("I ran this code at ", n)
+
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Println("Go launched at ", t)
+
+	fmt.Println(t.Format(time.ANSIC))
+
+	parsedTime, _ := time.Parse(time.ANSIC, "Tue NOv 10 23:00 2009")
+	fmt.Printf("The type of parsedTimee is %T\n", parsedTime)
 }
