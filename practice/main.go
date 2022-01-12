9ce273f1cdca0059b9cfaa2c2e2a2c1517b55a4b
loudgazelle <loudgazelle@users.noreply.github.com>
2022-01-12 17:11:27 -0600

    practice 04_01

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+
+	theAnswer := 42
+	var result string
+
+	if theAnswer < 0 {
+		result = "less than zero"
+	} else if theAnswer == 0 {
+		result = "equal to zero"
+	} else {
+		result = "greater than zero"
+	}
+	fmt.Println(result)
+
+	if x := -42; x < 0 {
+		result = "less than zero"
+	} else if x == 0 {
+		result = "equal to zero"
+	} else {
+		result = "greater than zero"
+	}
+	fmt.Println(result)
+
 }
