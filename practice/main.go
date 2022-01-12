ff6e355cbecd6d8694a4c7a7a3d7553d13c93dd3
loudgazelle <loudgazelle@users.noreply.github.com>
2022-01-12 16:19:23 -0600

    practice 03_03b

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("number of colors:", len(colors))
+	fmt.Println("number of numbers:", len(numbers))
+
 }
