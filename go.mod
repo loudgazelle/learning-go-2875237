001e7cae0b5ef8b8e82bc9739f12bb002a966b7e
loudgazelle <loudgazelle@users.noreply.github.com>
2022-01-12 17:41:12 -0600

    practice 04_02

---
+++ go.mod
@@ -0,0 +1,3 @@
+module origin
+
+go 1.17
--- practice/main.go
+++ practice/main.go
@@ -8,6 +8,24 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	// fmt.Println("Day", dow)
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "it's Sunday!"
+		// fallthrough
+	case 2:
+		result = "it's Monday!"
+		// fallthrough
+	case 3:
+		result = "it's Tuesday!"
+		// fallthrough
+	case 4:
+		result = "it's Wednesday!"
+		// fallthrough
+	default:
+		result = "it's some other day!"
+	}
+	fmt.Println(result)
 }
