69669ee274bce136209fc4421de5605060977235
loudgazelle <loudgazelle@users.noreply.github.com>
2022-01-13 11:24:17 -0600

    practice 05_01

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,29 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	fmt.Println(addValues(420, 69))
+	sum := addValues(5, 8)
+	fmt.Println("The sum is", sum)
+
+	multiSum, multiCount := addAllValues(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25)
+	fmt.Println("Sum of multiple values:", multiSum)
+	fmt.Println("count of iteems", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("doing something")
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
